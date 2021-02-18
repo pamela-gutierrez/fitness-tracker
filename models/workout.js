60656a88a110dc0ac41271e61d82a386d282aa9a
pmgtrz <75604818+pamela-gutierrez@users.noreply.github.com>
2021-02-17 23:05:50 -0800

    add workout model

---
+++ models/workout.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            require: "Type is required"
+        },
+        name: {
+            type: String,
+            trim: true,
+            require: "Name is required"
+        },
+        duration: {
+            type: Number,
+        },
+        weight: {
+            type: Number,
+        },
+        reps: {
+            type: Number,
+        },
+        sets: {
+            type: Number,
+        }
+    }]
+});
+
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
